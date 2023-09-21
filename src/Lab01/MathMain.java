b05be9057df12aea6e0eb82b4fa0e532bd382766
Huch0 <heochiyeong@gmail.com>
2023-09-21 16:32:34 +0900

    Refactor directory system
    - remove week2
    - add Lab01, Lab02

---
+++ src/Lab01/MathMain.java
@@ -0,0 +1,34 @@
+package week2;
+
+import java.util.Scanner;
+
+public class MathMain {
+    public static void main(String[] args) {
+        System.out.print("Enter two numbers: ");
+        final Scanner scanner = new Scanner(System.in);
+        final int begin = scanner.nextInt();
+        final int end = scanner.nextInt();
+        scanner.close();
+
+        final long sum = getSumBetween(begin, end);
+        System.out.printf("Sum between %d and %d : %,d%n", begin, end, sum);
+
+        final long product = getProductBetween(begin, end);
+        System.out.printf("Product between %d and %d : %,d%n", begin, end, product);
+    }
+
+    public static long getSumBetween(int begin, int end) {
+        long sum = 0L;
+        for (int i = begin; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    public static long getProductBetween(int begin, int end) {
+        long product = 1L;
+        for (int i = begin; i <= end; i++) {
+            product *= i;
+        }
+        return product;
+    }
+}
---
+++ src/Lab02/FactorialMain.java
@@ -0,0 +1,18 @@
+package week2;
+
+public class FactorialMain {
+    public static void main(String[] args) {
+        for ( int i = 1; i <= 10; i ++ ) {
+            final long fact = factorial(i);
+            System.out.printf("Factorial of %d: %d%n", i, fact);
+        }
+    }
+    private static long factorial(final int n) {
+        long fact = 1L;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
